647a6acb6b2c8db5b289a19260594fceddb05ecd
Gendzo <121614537+nGendzo@users.noreply.github.com>
2023-02-02 12:03:29 +0400

    Working calculator script and logic

--- script.js
+++ script.js
@@ -1,44 +1,69 @@
+let history = document.querySelector('#history');
 const display = document.querySelector('#display');
 const clear = document.querySelector('#clear');
 const numberButtons = [...document.querySelectorAll('.number-button')];
 const operatorButtons = [...document.querySelectorAll('.operator')]; 
 const equalButton = document.querySelector('#equal');
+const clearButton = document.querySelector('#clear');
 
-
-let lastInput = 0;
+let firstInput = 0;
+let secondInput = 0;
 let operatorInput;
 
 numberButtons.forEach(element => {
     element.addEventListener('click', () => {
-        display.textContent = element.textContent;
-        lastInput = element.textContent;
-        lastInput *= 1;
-        console.log(lastInput);
+        display.textContent += element.textContent;
     });
 });
 
 operatorButtons.forEach(element => {
     element.addEventListener('click', () => {
+
+        if (history.textContent != ""){
+            equal();
+        }
+
+        firstInput = display.textContent * 1;
+        history.textContent = firstInput;
         operatorInput = element.textContent;
-        console.log(operatorInput);
-    })
-})
+        history.textContent += operatorInput;
+        display.textContent = "";
+    });
+});
+
+equalButton.addEventListener('click', equal);
+
+clearButton.addEventListener('click', () => {
+    firstInput = 0;
+    secondInput = 0;
+    display.textContent = "";
+    history.textContent = "";
+});
+
+function equal(){
+    secondInput = display.textContent * 1;
+    let sum = operate(operatorInput, firstInput, secondInput);
+    history.textContent = "";
+    display.textContent = sum;
+    firstInput = 0;
+    secondInput = 0;
+}
 
 function add(a, b){
     return a + b;
-}
+};
 
 function subtract(a, b){
     return a - b;
-}
+};
 
 function multiply(a, b){
     return a * b;
-}
+};
 
 function divide(a, b){
     return a / b;
-}
+};
 
 function operate(operator, num1, num2){
     if (operator == "+"){
@@ -47,7 +72,7 @@ function operate(operator, num1, num2){
         return subtract(num1, num2);
     }else if (operator == "*"){
         return multiply(num1, num2);
-    }else if (operator == "/"){
+    }else if (operator == "%"){
         return divide(num1, num2);
-    }
-}
+    };
+};
